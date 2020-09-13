fbce433fc78802902b79725fb8e2e7cb9322bdff
Napo <Michaeljnapolitano@gmail.com>
2020-09-12 22:59:55 -0400

    api fetch

---
+++ assets/logic.js
@@ -0,0 +1,29 @@
+var searchInput = document.querySelector("#search-input")
+var findCity = document.querySelector("#search-btn")
+var listItemE1 = document.querySelector(".list-group")
+var cityCard = document.querySelector("#city-container")
+
+$(document).ready(function() {
+
+var getWeatherInfo = function(city) {
+
+    // format the github api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=f28282748979d8ef4250a43282c46535";
+
+    // make a repo request to the url
+    fetch(apiUrl).then(function(repsonse) {
+        repsonse.json().then(function(data) {
+            var latitude = data.city.coord.lat
+            var longitude = data.city.coord.lon
+            return fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=f28282748979d8ef4250a43282c46535").then(function(uvResponse) {
+                uvResponse.json().then(function(uvData) {
+                    displayWeather(data, city, uvData);
+                    console.log(data, uvData)
+                })
+            })
+        });
+    });
+};
+
+
+
